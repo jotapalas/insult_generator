e56a6323535ca421b6bb79ed39eb1075bc183a05
Juan Palacios <palaciosgalvanj@gmail.com>
2019-02-28 15:15:55 +0100

    Added multiple insult option

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from markov_python.cc_markov import MarkovChain
 from fetch_data import get_insults
 
+import sys
+from getopt import getopt
+
 def format(str):
   out = str[0].capitalize()
   for i in range(1, len(str)):
@@ -17,7 +20,18 @@ for insult in insults:
   mc_insults.add_string(insult[0])
   mc_comebacks.add_string(insult[1])
 
-generated_insult = mc_insults.generate_text(100)
-generated_comeback = mc_comebacks.generate_text(100)
+count = 1
+optlist, args = getopt(sys.argv[1:], 'c:')
+for arg in optlist:
+  if arg[0] == '-c':
+    count = int(arg[1])
+    break;
+else:
+  if len(args) > 0:
+    count = int(args[0])
 
-print ("- %s\n- %s" % (format(generated_insult), format(generated_comeback)))
+while count > 0:
+  generated_insult = mc_insults.generate_text(100)
+  generated_comeback = mc_comebacks.generate_text(100)
+  print ("- %s\n- %s\n" % (format(generated_insult), format(generated_comeback)))
+  count -= 1
