02226c51f27e00f866e69a7c1a4af02f9800b980
Juan Palacios Galván <palaciosgalvanj@gmail.com>
2019-02-28 17:22:20 +0100

    Fixed minor bugs recovering insults

--- fetch_data.py
+++ fetch_data.py
@@ -5,18 +5,29 @@ def get_insults():
   r = get_request('https://monkeyisland.fandom.com/wiki/Insult_Sword_Fighting')
 
   if r.status_code == 200:
+    insults = []
     parsed_html = BeautifulSoup(r.text)
-    html_tables = parsed_html.body.findAll('table', attrs={
+    table1 = parsed_html.body.find('table', attrs={
       'class' : 'article-table'
     })
+    table2 = parsed_html.body.find('table', attrs={
+      'class' : 'article-table article-table-selected'
+    })
+    table3 = parsed_html.body.findAll('table', attrs={
+      'class' : 'article-table'
+    })[1]
 
-    insults = []
-    for table in html_tables:
-      if table.find('th').text == 'Insult':
-        for row in table.findAll('tr'):
-          columns = row.findAll('td')
-          if(len(columns) > 0):
-            insults.append([columns[0].text, columns[1].text])
-    
+    insults.extend(get_insults_from_table(table1, 0, 1))
+    insults.extend(get_insults_from_table(table2, 0, 1))
+    insults.extend(get_insults_from_table(table3, 0, 2))
+    insults.extend(get_insults_from_table(table3, 1, 2))
     return insults
-    
+
+def get_insults_from_table(table, insult_column, comeback_column):
+  insults = []
+  if table.find('th').text == 'Insult':
+      for row in table.findAll('tr'):
+        columns = row.findAll('td')
+        if(len(columns) > 0):
+          insults.append([columns[insult_column].text, columns[comeback_column].text])
+  return insults
